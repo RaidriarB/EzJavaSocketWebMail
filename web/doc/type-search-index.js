aadc5aacef2607c55fe513b94e22818e9b9881cc
raidriarb <raidriarb@foxmail.com>
2019-12-18 00:24:43 +0800

    删除了没用的JavaDoc

--- web/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"HttpObject"},{"p":"<Unnamed>","l":"HTTPParser"},{"p":"<Unnamed>","l":"HTTPRespObject"},{"p":"<Unnamed>","l":"HTTPServerThread"},{"p":"<Unnamed>","l":"MainServer"},{"p":"<Unnamed>","l":"SMTPClient"},{"p":"<Unnamed>","l":"SMTPFunction"}]
